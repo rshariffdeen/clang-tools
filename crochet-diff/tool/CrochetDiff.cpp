7095513f6a97a41ef6d279908146bc7f480d7365
rshariffdeen <rshariffdeen@gmail.com>
2020-02-17 16:39:56 +0800

    print file for initlistexpr

--- crochet-diff/tool/CrochetDiff.cpp
+++ crochet-diff/tool/CrochetDiff.cpp
@@ -365,7 +365,7 @@ static void printNodeAttributes(raw_ostream &OS, diff::SyntaxTree &Tree,
     OS << R"(,"parent_id":)" << int(Node.getParent()->getId());
   OS << R"(,"type":")" << Node.getTypeLabel() << '"';
 
-  if (Node.getTypeLabel() == "FunctionDecl" || Node.getTypeLabel() == "TypedefDecl" || Node.getTypeLabel() == "RecordDecl"
+  if (Node.getTypeLabel() == "FunctionDecl" || Node.getTypeLabel() == "TypedefDecl" || Node.getTypeLabel() == "RecordDecl" || Node.getTypeLabel() == "InitListExpr"
       || Node.getTypeLabel() == "EnumDecl" || Node.getTypeLabel() == "EnumConstantDecl" || Node.getTypeLabel() == "Macro" || Node.getTypeLabel() == "TypedefDecl" || Node.getTypeLabel() == "VarDecl" ){
     std::string fileName = Node.getFileName();
       if (!fileName.empty()) {
