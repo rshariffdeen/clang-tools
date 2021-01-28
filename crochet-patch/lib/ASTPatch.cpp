194c0fb9812fcfb5574360163b51bd9f01db81b9
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-01-28 18:03:00 +0800

    translate macro functions

--- crochet-patch/lib/ASTPatch.cpp
+++ crochet-patch/lib/ASTPatch.cpp
@@ -804,6 +804,9 @@ namespace clang {
 
                 std::string refNameInSource = node.getValue();
 
+                if (refNameInSource.find("(")) != std::string::nops)
+                    refNameInSource = refNameInSource.substr(0, refNameInSource.find("(")) + "(";
+
                 // llvm::outs() << "var name in source: " << variableNameInSource << "\n";
                 //  llvm::outs() << "before translation: " << statement << "\n";
                 std::string refNameInTarget;
